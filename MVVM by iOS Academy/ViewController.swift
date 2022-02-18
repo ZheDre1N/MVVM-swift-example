c686f069bdbdc4022cc7b8e0c0622789c619083a
Eugene Dudkin <ZheDre1N@gmail.com>
2022-02-18 21:30:06 +0300

    Initial Commit

---
+++ MVVM by iOS Academy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM by iOS Academy
+//
+//  Created by Eugene Dudkin on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
